95f43e54b8f78b68ed2a71d695b11110c9a8084e
dianajchoi <dianajchoi@gmail.com>
2019-11-04 13:45:53 -0800

    initial commit
    only has skeleton code for ChatServer.java and misc. eclipse files

---
+++ src/ChatServer.java
@@ -0,0 +1,20 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.concurrent.ConcurrentHashMap;
+/*
+ * File: ChatServer.java
+ * Author: Diana Choi, Sara Joshi
+ * Class: CS 3800-02 Computer Networks
+ * Professor: J. Korah
+ * Assignment: Final
+ * 
+ * 
+ * Purpose: This class is as a server that listens for
+ *          connections
+ */
+public class ChatServer {
+
+}
